b08f6bc0abb91744ccdda7c691a35dffdfbda7af
wallace <yunhua.lee@gmail.com>
2021-06-08 11:15:49 +0800

    add bin script

---
+++ bin/start-master.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -jar ../target/queue-demo-1.0-SNAPSHOT.jar -p8080 -Sarch -gdemo -z127.0.0.1 -m
---
+++ bin/start-slave.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -jar ../target/queue-demo-1.0-SNAPSHOT.jar -p8081 -Sarch -gdemo -z127.0.0.1 -s
